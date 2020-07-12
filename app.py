7daf164f8a2562fa670f1458a1705baf05864556
Alvis F <falvis1999@gmail.com>
2020-07-12 15:29:36 +0530

    High chart started

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask, render_template, g
+from flask import Flask, render_template, make_response
 import sys
 import logging
-
+from time import time
+import json
+from random import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
@@ -13,7 +15,7 @@ app.logger.setLevel(logging.ERROR)
 
 @app.route('/')
 def index():
-    
+
     countries = [
         ["India", "Asia"],
         ["Canada", "North America"]
@@ -21,3 +23,24 @@ def index():
 
     return render_template('index.html', countries=countries)
 
+
+@app.route('/data', methods=["GET", "POST"])
+def data():
+    # Data Format
+    # [TIME, Temperature, Humidity]
+
+    Temperature = random() * 100
+    Humidity = random() * 55
+
+    data = [time() * 1000, Temperature, Humidity]
+
+    response = make_response(json.dumps(data))
+
+    response.content_type = 'application/json'
+
+    return response
+
+
+@app.route('/highchart', methods=["GET", "POST"])
+def main():
+    return render_template('graph.html')
